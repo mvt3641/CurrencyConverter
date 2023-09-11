5147e65fbf5addf2f645d9b88241bb975f1d5614
Matt <mvt3641@gmail.com>
2023-09-11 19:24:42 +0100

    draft template; need apis

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ from flask import Flask, render_template
 app = Flask(__name__) # Folder Name
 
 @app.route('/')
-def index(): # App main function
+def index(): # App main page function
     return render_template('index.html')
-  
+
+@app.route('/sample',methods=["GET","POST"] )
+def sample(): # App sample conversion form
+    currencies = ['Dinar1', 'Dinar2', 'Dinar3', 'Dollar', 'Pound', 'etc']
+    return render_template('sample.html',currencies=currencies)
+
+@app.route('/about')
+def about(): # App about or instructions form
+    return render_template('about.html')
 
 if __name__=="__main__":
     app.run(debug=True)
